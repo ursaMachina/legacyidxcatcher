54eca4580fe329ece0f48119601a476c14f9678a
Dnktheledge <37371318+ursaMachina@users.noreply.github.com>
2018-04-13 15:54:03 +0100

    Create legidxcatch.sh

---
+++ legidxcatch.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#set the location of the list of index names, a file with an index name per new line.
+LIST_FILE="./legidxfile"
+
+#set the location of the outputfile/name.
+TSFMS="./transforms.conf"
+PROPS="./props.conf"
+RE1="(\w+),"
+  echo "Starting."
+  IFS=$'\n'
+  for DST in `cat "$LIST_FILE"`; do
+    if [ -z "$DST" ]; then
+      continue;
+    fi
+    var=$(echo $DST|awk '{print $1}')
+    var2=$(echo $DST|awk '{print $2}')
+
+    echo "[$var""_legtsfm]" >> $TSFMS
+    echo "SOURCE_KEY = _MetaData:Index" >> $TSFMS
+    echo "REGEX = $var" >> $TSFMS
+    echo "DEST_KEY = _MetaData:Index" >> $TSFMS
+    echo "FORMAT = $var2" >> $TSFMS
+    echo "" >> $TSFMS
+   done  
+  echo "Finished"
